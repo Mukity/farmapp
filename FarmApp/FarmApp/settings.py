bfef2e34a365303d2d2c59f19e41f4467aee3c47
Mukity <jacob97mukity@gmail.com>
2021-04-14 20:42:23 +0100

    I know you did it

--- FarmApp/FarmApp/settings.py
+++ FarmApp/FarmApp/settings.py
@@ -67,13 +67,8 @@ WSGI_APPLICATION = 'FarmApp.wsgi.application'
 
 DATABASES = {
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'FARMAPP',
-        'USER': 'farmapp',
-        'PASSWORD': 'farmapp2021',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
